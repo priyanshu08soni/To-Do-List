6121a06cea7ebc4af1b7091e67c66dc19a4c4f1e
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2024-05-26 07:11:34 +0530

    dbchange

--- To-Do-List-backend/config/dbConnect.js
+++ To-Do-List-backend/config/dbConnect.js
@@ -1,5 +1,4 @@
 const {default:mongoose}=require("mongoose");
-let uri=`mongodb+srv://priyanshus20k4:Priyanshu%40834@mycluster.dnwjrp2.mongodb.net/ToDoList?retryWrites=true&w=majority&appName=MyCluster`
 const dbConnect=()=>{
     try {
         const conn=mongoose.connect('mongodb://localhost:27017/Task');
